d4e92c5c2fdb4c46841ee697f62001f2b9af8c48
Radu <radu.stanciu2016@gmail.com>
2024-05-03 15:26:05 +0300

    made functions for getting computer and human choices

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello World")
+const rps = ["rock", "paper", "scissors"];
+
+function randomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+function inRps(choice) {
+  choice = String(choice).toLowerCase();
+  for(let i = 0; i < 3; i++) {
+    if (rps[i] == choice) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getComputerChoice() {
+  let choice = rps[randomInt(0, 2)];
+  return choice;
+}
+
+function getHumanChoice() {
+  let choice = " ";
+  while(inRps(choice) === false) {
+    choice = prompt("Rock, paper or scissors?");
+  }
+  return choice;
+}
