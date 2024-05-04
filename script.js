ac2db8c0bec96b6819aadc83269a892bf650cfd7
Radu <radu.stanciu2016@gmail.com>
2024-05-04 14:18:23 +0300

    Built playRound function to determine winner between human choice and computer choice

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const rps = ["rock", "paper", "scissors"];
 
 const humanScore = 0;
-comst computerScore = 0;
+const computerScore = 0;
 
 function randomInt(min, max) {
   const minCeiled = Math.ceil(min);
@@ -32,5 +32,50 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-  
+  let winner = " ";
+  let message = " ";
+  console.log("human choice: " + humanChoice);
+  console.log("computer choice: " + computerChoice);
+  for (let i = 0; i < 3; i++) {
+    if(humanChoice == rps[i]) {
+      for (let j = 0; j < 3; j++) {
+        if(computerChoice == rps[j]) {
+          if(i == j) {
+            //winner = "draw";
+            message = "Draw! Both chose " + rps[i];
+            console.log(message);
+            return;
+          }
+
+          if(j+1 == 3) {
+            j -= 3;
+          }
+          if(i == j+1) {
+            //winner = "human";
+            if(j < 0){
+              j += 3;
+            }
+            message = "You win! " + rps[i] + " beats " + rps[j];
+            console.log(message);
+            return;
+          }
+
+          if(j+2 >= 3) {
+            j -= 3;
+          }
+          if(i == j+2) {
+            //winner = "computer";
+            if(j < 0){
+              j += 3;
+            }
+            message = "You lose! " + rps[j] + " beats " + rps[i];
+            console.log(message);
+            return;
+          }
+        }
+      }
+    }
+  }
 }
+
+playRound(getHumanChoice(), getComputerChoice());
