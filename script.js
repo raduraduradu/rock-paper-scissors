e3f9d7a8e6f29d93f1f774ec5fba032b016be126
Radu <radu.stanciu2016@gmail.com>
2024-05-06 16:10:38 +0300

    Made playGame function, completed project for now
    will add an ui later on and probably rework the code

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const rps = ["rock", "paper", "scissors"];
 
-const humanScore = 0;
-const computerScore = 0;
 
 function randomInt(min, max) {
   const minCeiled = Math.ceil(min);
@@ -31,51 +29,74 @@ function getHumanChoice() {
   return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-  let winner = " ";
-  let message = " ";
-  console.log("human choice: " + humanChoice);
-  console.log("computer choice: " + computerChoice);
-  for (let i = 0; i < 3; i++) {
-    if(humanChoice == rps[i]) {
-      for (let j = 0; j < 3; j++) {
-        if(computerChoice == rps[j]) {
-          if(i == j) {
-            //winner = "draw";
-            message = "Draw! Both chose " + rps[i];
-            console.log(message);
-            return;
-          }
 
-          if(j+1 == 3) {
-            j -= 3;
-          }
-          if(i == j+1) {
-            //winner = "human";
-            if(j < 0){
-              j += 3;
-            }
-            message = "You win! " + rps[i] + " beats " + rps[j];
-            console.log(message);
-            return;
-          }
+//playRound(getHumanChoice(), getComputerChoice());
 
-          if(j+2 >= 3) {
-            j -= 3;
-          }
-          if(i == j+2) {
-            //winner = "computer";
-            if(j < 0){
-              j += 3;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  function playRound(humanChoice, computerChoice) {
+    let winner = " ";
+    //console.log("human choice: " + humanChoice);
+    //console.log("computer choice: " + computerChoice);
+    for (let i = 0; i < 3; i++) {
+      if(humanChoice == rps[i]) {
+        for (let j = 0; j < 3; j++) {
+          if(computerChoice == rps[j]) {
+            if(i == j) {
+              //winner = "draw";
+              message = `Draw! Both chose ${rps[i]}\n (h: ${humanScore}, c:${computerScore})`;
+              console.log(message);
+              return;
             }
-            message = "You lose! " + rps[j] + " beats " + rps[i];
-            console.log(message);
-            return;
+
+            if(j+1 == 3) {
+              j -= 3;
+            }
+            if(i == j+1) {
+              //winner = "human";
+              if(j < 0){
+                j += 3;
+              }
+              humanScore++;
+              message = `You win! ${rps[i]} beats ${rps[j]}\n (h: ${humanScore}, c:${computerScore})`;
+              console.log(message);
+              return;
+            }
+
+            if(j+2 >= 3) {
+              j -= 3;
+            }
+            if(i == j+2) {
+              //winner = "computer";
+              if(j < 0){
+                j += 3;
+              }
+              computerScore++;
+              message = `You lose! ${rps[j]} beats ${rps[i]}\n (h: ${humanScore}, c:${computerScore})`;
+              console.log(message);
+              return;
+            }
           }
         }
       }
     }
   }
+
+  for (let r = 0; r < 5; r++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+  if(humanScore > computerScore) {
+  console.log(`Final winner: you (h: ${humanScore}, c:${computerScore})`);
+  }
+  
+  else if (computerScore > humanScore) {
+    console.log(`Final winner: computer (h: ${humanScore}, c:${computerScore})`);
+  }
+  
+  else {
+    console.log(`Draw! (tied on score ${humanScore})`);
+  }
 }
 
-playRound(getHumanChoice(), getComputerChoice());
+playGame();
