ff6edf0f35b0382500d7b5aa4367b926c248ddf5
Radu <radu.stanciu2016@gmail.com>
2024-05-11 16:30:58 +0300

    Made working buttons, results displaying on UI instead of console, declares winner at 5 points

--- script.js
+++ script.js
@@ -38,45 +38,78 @@ function getHumanChoice() {
   return choice;
 }
 
-function playGame() {
 
-  let humanScore = 0;
-  let computerScore = 0;
-  
-  function playRound(humanChoice, computerChoice) {
-    if(computerChoice == loser.get(humanChoice)) {
-      humanScore++;
-      console.log(`You win! ${humanChoice} beats ${computerChoice}\n (h: ${humanScore}, c:${computerScore})`);
-      return;
-    }
-    else if (humanChoice == loser.get(computerChoice)) {
-      computerScore++;
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}\n (h: ${humanScore}, c:${computerScore})`);
-      return;
-    }
-    else {
-      console.log(`Draw! Both chose ${humanChoice}\n (h: ${humanScore}, c:${computerScore})`);
-      return;
-    }
+let humanScore = 0;
+let computerScore = 0;
+let textdiv = document.querySelector("div#text");
+
+function playRound(humanChoice, computerChoice) {
+  if(humanScore > 4 || computerScore > 4) {
+    declareFinalWinner()
+    return;
   }
- 
+
+  if(computerChoice == loser.get(humanChoice)) {
+    humanScore++;
+    //console.log(`You win! ${humanChoice} beats ${computerChoice}\n (h: ${humanScore}, c:${computerScore})`);
+    textdiv.style.color = "Green";
+    textdiv.textContent = `You win! ${humanChoice} beats ${computerChoice}\n (h: ${humanScore}, c:${computerScore})`;
+    return;
+  }
+  else if (humanChoice == loser.get(computerChoice)) {
+    computerScore++;
+    //console.log(`You lose! ${computerChoice} beats ${humanChoice}\n (h: ${humanScore}, c:${computerScore})`);
+    textdiv.style.color = "red";
+    textdiv.textContent = `You lose! ${computerChoice} beats ${humanChoice}\n (h: ${humanScore}, c:${computerScore})`;
+    return;
+  }
+  else {
+    //console.log(`Draw! Both chose ${humanChoice}\n (h: ${humanScore}, c:${computerScore})`);
+    textdiv.style.color = "orange";
+    textdiv.textContent = `Draw! Both chose ${humanChoice}\n (h: ${humanScore}, c:${computerScore})`;
+    return;
+  }
+  
+}
+
+function declareFinalWinner(){
+  const htmlBody = document.querySelector("body");
+  const finalMessage = document.createElement("div");
+
+  htmlBody.prepend(finalMessage);
+
   if(humanScore > computerScore) {
-  console.log(`Final winner: you (h: ${humanScore}, c:${computerScore})`);
+  //console.log(`Final winner: you (h: ${humanScore}, c:${computerScore})`);
+    finalMessage.textContent = `Final winner: you (h: ${humanScore}, c:${computerScore})`
   }
   
   else if (computerScore > humanScore) {
-    console.log(`Final winner: computer (h: ${humanScore}, c:${computerScore})`);
+    //console.log(`Final winner: computer (h: ${humanScore}, c:${computerScore})`);
+    finalMessage.textContent = `Final winner: computer (h: ${humanScore}, c:${computerScore})`;
   }
   
   else {
-    console.log(`Draw! (tied on score ${humanScore})`);
+    //console.log(`Draw! (tied on score ${humanScore})`);
+    finalMessage.textContent = `Draw! (tied on score ${humanScore})`;
   }
-}
+
+  btnRock.disabled = true;
+  btnPaper.disabled = true;
+  btnScissors.disabled = true;
+}  
 
 
-btn-rock = document.querySelector("#btn-rock");
-btn-rock.addEventListener("click", () => {
-  playRound("rock", getComputerChoice);
+const btnRock = document.querySelector("#btn-rock");
+btnRock.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
 });
 
-playGame();
+const btnPaper = document.querySelector("#btn-paper");
+btnPaper.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
+
+const btnScissors = document.querySelector("#btn-scissors");
+btnScissors.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+});
