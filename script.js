a3f028156935464c21ebf051ff4d94262cf7bc0a
Radu <radu.stanciu2016@gmail.com>
2024-05-11 16:38:32 +0300

    Removed unused functions getHumanChoice(prompts user for input) and inRps(checks if user input is valid)

--- script.js
+++ script.js
@@ -14,27 +14,8 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
-function inRps(choice) {
-  for(let i = 0; i < 3; i++) {
-    if (rps[i] == choice) {
-      return true;
-    }
-  }
-  return false;
-}
-
 function getComputerChoice() {
   let choice = rps[randomInt(0, 2)];
-  return choice;
-}
-
-function getHumanChoice() {
-  let choice = String(prompt("Rock, paper or scissors?")).toLowerCase();
-
-  while(inRps(choice) === false) {
-    alert(`${choice} is not valid input`);
-    choice = String(prompt("Rock, paper or scissors?")).toLowerCase();
-  }
   return choice;
 }
 
@@ -87,12 +68,7 @@ function declareFinalWinner(){
     //console.log(`Final winner: computer (h: ${humanScore}, c:${computerScore})`);
     finalMessage.textContent = `Final winner: computer (h: ${humanScore}, c:${computerScore})`;
   }
-  
-  else {
-    //console.log(`Draw! (tied on score ${humanScore})`);
-    finalMessage.textContent = `Draw! (tied on score ${humanScore})`;
-  }
-
+ 
   btnRock.disabled = true;
   btnPaper.disabled = true;
   btnScissors.disabled = true;
