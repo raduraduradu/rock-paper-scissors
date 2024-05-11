300884079376832a3f2e7936efb03b0d35a45852
Radu <radu.stanciu2016@gmail.com>
2024-05-11 20:59:04 +0300

    Added header, footer, title, score and tweaked a few other things

--- script.js
+++ script.js
@@ -22,52 +22,49 @@ function getComputerChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+const scoreDiv = document.querySelector("#score");
 let textdiv = document.querySelector("div#text");
 //textdiv.style.backgroundColor = "#555";
 
 function playRound(humanChoice, computerChoice) {
-  if(humanScore > 4 || computerScore > 4) {
-    declareFinalWinner()
-    return;
-  }
 
   if(computerChoice == loser.get(humanChoice)) {
     humanScore++;
     //console.log(`You win! ${humanChoice} beats ${computerChoice}\n (h: ${humanScore}, c:${computerScore})`);
     textdiv.style.color = "Green";
-    textdiv.textContent = `You win! ${humanChoice} beats ${computerChoice}\n (h: ${humanScore}, c:${computerScore})`;
-    return;
+    textdiv.textContent = `You win! ${humanChoice} beats ${computerChoice}\n`;
   }
   else if (humanChoice == loser.get(computerChoice)) {
     computerScore++;
     //console.log(`You lose! ${computerChoice} beats ${humanChoice}\n (h: ${humanScore}, c:${computerScore})`);
     textdiv.style.color = "#f33";
-    textdiv.textContent = `You lose! ${computerChoice} beats ${humanChoice}\n (h: ${humanScore}, c:${computerScore})`;
-    return;
+    textdiv.textContent = `You lose! ${computerChoice} beats ${humanChoice}\n`;
   }
   else {
     //console.log(`Draw! Both chose ${humanChoice}\n (h: ${humanScore}, c:${computerScore})`);
     textdiv.style.color = "orange";
-    textdiv.textContent = `Draw! Both chose ${humanChoice}\n (h: ${humanScore}, c:${computerScore})`;
+    textdiv.textContent = `Draw! Both chose ${humanChoice}\n`;
+  }
+  scoreDiv.textContent = `you: ${humanScore} --- computer: ${computerScore}`;
+  if(humanScore > 4 || computerScore > 4) {
+    declareFinalWinner()
     return;
   }
-  
 }
 
 function declareFinalWinner(){
   //const htmlBody = document.querySelector("body");
-  const finalMessage = document.querySelector("#final-message");
-
+  
   if(humanScore > computerScore) {
   //console.log(`Final winner: you (h: ${humanScore}, c:${computerScore})`);
-    finalMessage.style.color = "green";
-    finalMessage.textContent = `Final winner: you (h: ${humanScore}, c:${computerScore})`
+    scoreDiv.style.color = "green";
+    scoreDiv.textContent = `Final winner: you (h: ${humanScore}, c:${computerScore})`
   }
   
   else if (computerScore > humanScore) {
     //console.log(`Final winner: computer (h: ${humanScore}, c:${computerScore})`);
-    finalMessage.style.color = "#f33";
-    finalMessage.textContent = `Final winner: computer (h: ${humanScore}, c:${computerScore})`;
+    scoreDiv.style.color = "#f33";
+    scoreDiv.textContent = `Final winner: computer (h: ${humanScore}, c:${computerScore})`;
   }
  
   btnRock.disabled = true;
